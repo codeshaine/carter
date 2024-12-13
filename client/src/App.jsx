fe7fc07e21a0156a6513d2bdee3cd6ef4ca60880
codeshaine <shainilps05@gmail.com>
2024-12-13 21:40:41 +0530

    updated: backend url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,11 +28,11 @@ import GlobalError from "./pages/GlobalError";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK);
+const stripePromise = loadStripe("pk_test_51QAvqm00g9VRghb1v0IbzQ3FGWjBYQf9pkSqsw2Q7oW0unOymv3aUob6dCIVfYXZtnOC9VKhYgSBNZdL9qrWSi4V00VM11rNyU");
 
 function App() {
   axios.defaults.withCredentials = true;
-  axios.defaults.baseURL = import.meta.env.VITE_BACKEND_API_URL;
+  axios.defaults.baseURL = "https://carter-me2x.onrender.com";
   const router = createBrowserRouter(
     createRoutesFromElements([
       <Route key="global" path="/">
