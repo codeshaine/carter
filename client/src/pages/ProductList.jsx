86632c3d18bde043a2aa483ac773b4f5094fa1e2
codeshaine <shainilps05@gmail.com>
2024-12-14 11:07:10 +0530

    Fix: fixed the pl

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -180,7 +180,7 @@ function ProductList() {
               &quot;
             </h1>
           </div>
-          {products.pl?.length > 0 ? (
+          {products?.pl?.length > 0 ? (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
               {products.pl.map((product) => (
                 <div
